f1f1d89f91bfae9c56c8387c4dd00b7a411b336f
Alex <dinhhungvn@gmail.com>
2023-04-03 10:49:26 +0700

    Update css

--- src/components/FordeerAdv/FordeerAdv.tsx
+++ src/components/FordeerAdv/FordeerAdv.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 const FordeerAdv = () => {
   return (
-    <div>
+    <div className="adv">
       <a href="https://link.fordeer.io/BGBdhP">
         <img src="https://res-2.cloudinary.com/fordeer/image/upload/q_auto/v1/blog-images/banner-1.png"/>
       </a>
